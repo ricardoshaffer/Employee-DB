a7a53668ed62128a40f8ce968314dd2376719644
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-04-15 18:43:11 -0400

    added heroku update

--- internal/serverLink.js
+++ internal/serverLink.js
@@ -1,5 +1,17 @@
 const mysql = require("mysql");
+let serverLink;
 
+if(process.env.JAWSDB_URL){
+    serverLink = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    serverLink = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "r!c@rd01987",
+        database: "employee_manager"
+    })
+}
 let serverLink = mysql.createConnection({
     host: "localhost",
     port: 3306,
